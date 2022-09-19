a35b86e57d0482ce484da3d16532e23bec3f8925
Sailormw <109222967+Sailormw@users.noreply.github.com>
2022-09-19 21:25:16 +0700

    Update main-menu1.js

--- plugins/main-menu1.js
+++ plugins/main-menu1.js
@@ -3,7 +3,7 @@ let moment = require('moment-timezone')
 let time = moment.tz('Asia/Jakarta').format("HH:mm:ss")
 let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 
-conn.sendButtonDoc(m.chat, 'Hai, Namaku QwertyBot, Aku diciptakan untuk membantu dan menghibur kalian, orang yang mengembangkan ku salah satunya adalah Mukhammad Alfin Hidayatulloh. Oh iya, Jangan spam comment yah!!!', data.namabot, 'ʟɪꜱᴛᴍᴇɴᴜ', '.m', fake, { contextInfo: {
+conn.sendButtonDoc(m.chat, 'Hai, Namaku QwertyBot, Aku diciptakan untuk membantu dan menghibur kalian, orang yang mengembangkan ku salah satunya adalah AlfinQwe. Oh iya, Jangan spam comment yah!!!', data.namabot, 'ʟɪꜱᴛᴍᴇɴᴜ', '.m', fake, { contextInfo: {
 externalAdReply :{
     showAdAttribution: true,
     mediaUrl: data.sc,
